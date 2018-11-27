2a3af547e4b9adbdb513082410ecf6c3d29fca14
Nguyen Phuc Thien An <npta97@gmail.com>
2018-11-27 11:05:05 +0700

    Init Mongoose

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const routes = require('./routes');
 const config = require('./config');
 
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+mongoose.connect(config.mongoURI, { useNewUrlParser: true });
+
 app.use(bodyParser.json());
+
 app.use(morgan('dev'));
+
 app.use('/api', routes);
 
 app.listen(config.port, () => {
